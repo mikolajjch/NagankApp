abcad1738d4544ff21c587ee945861f0878a671b
mikolajjch <chrzanmikolaj@gmail.com>
2026-01-04 21:06:16 +0100

    add haversine and nearest point functions

---
+++ map-app/src/services/mathFun.ts
@@ -0,0 +1,21 @@
+const R = 6371000; //promień ziemii w metrach
+
+export function haversineDistance(
+  lat1: number,
+  lng1: number,
+  lat2: number,
+  lng2: number
+): number {
+  const toRad = (deg: number) => (deg * Math.PI) / 180;
+
+  const dLat = toRad(lat2 - lat1);
+  const dLng = toRad(lng2 - lng1);
+
+  const a =
+    Math.sin(dLat / 2) ** 2 +
+    Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * Math.sin(dLng / 2) ** 2;
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c;
+}
---
+++ map-app/src/services/nearestPath.ts
@@ -0,0 +1,24 @@
+import type { RoutePoint } from "../types/Route";
+import { haversineDistance } from "./mathFun";
+
+export function findNearestPoint(
+  userLat: number,
+  userLng: number,
+  points: RoutePoint[]
+) {
+  let minDistance = Infinity;
+  let nearest: RoutePoint | null = null;
+
+  for (const p of points) {
+    const d = haversineDistance(userLat, userLng, p.lat, p.lng);
+    if (d < minDistance) {
+      minDistance = d;
+      nearest = p;
+    }
+  }
+
+  return nearest ? { point: nearest, distance: minDistance } : null;
+}
+
+// potem jakakolwiek część ścieżki albo rozbić ścieżkę na więcej punktów
+// ...........
